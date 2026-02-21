8851f1c469f6527e4724e6136828d32d708d9126
Pranshul <threjapranshul@gmail.com>
2026-02-21 08:00:27 +0530

    Understanding the concept of Type Conversion & Autommated Type Prommotion in Java

---
+++ Variables/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+    // Data Type 
+    // Integer => Byte//Short//Int//Long
+    byte a=10;
+    short b=15000;
+    int c=200000;
+    long d=25000000;
+    System.out.println("Byte: "+ a + " Short: " +b + " Int: " +c + "  Long :" +d);
+    // Float => float//double
+    float e=15.098763f;
+    double f=25.964235821455;
+    System.out.println("Float: "+e + " Double: " +f );
+    // Character => Character
+    char g='p';
+    System.out.println("Char: "+g);
+    // Boolean => Bool
+    boolean h=true; //false
+    System.out.println("Boolean: "+h);
+    }
+}
